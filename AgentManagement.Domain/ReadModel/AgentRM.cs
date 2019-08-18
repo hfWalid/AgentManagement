903aed6234db7437dcf833944f7f26d3962fe7a6
eMateyWalid <walidhefhaf@gmail.com>
2019-08-18 18:51:36 +0100

    Representing classes of how the Locations and Agents will be stored in the RM database.

---
+++ AgentManagement.Domain/ReadModel/AgentRM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AgentManagement.Domain.ReadModel
+{
+    public class AgentRM
+    {
+        public int AgentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string JobTitle { get; set; }
+        public int LocationID { get; set; }
+        public Guid AggregateID { get; set; }
+    }
+}
---
+++ AgentManagement.Domain/ReadModel/LocationRM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgentManagement.Domain.ReadModel
+{
+    public class LocationRM
+    {
+        public int LocationID { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public List<int> Agents { get; set; }
+        public Guid AggregateID { get; set; }
+
+        public LocationRM()
+        {
+            Agents = new List<int>();
+        }
+    }
+}

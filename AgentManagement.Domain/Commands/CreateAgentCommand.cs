14d5d88271b3e3025a8e2d61d9322a4128b27c04
eMateyWalid <walidhefhaf@gmail.com>
2019-08-18 18:39:19 +0100

    Command dealing with the creation of a new Agent object.

---
+++ AgentManagement.Domain/Commands/CreateAgentCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AgentManagement.Domain.Commands
+{
+    public class CreateAgentCommand : BaseCommand
+    {
+        public readonly int AgentID;
+        public readonly string FirstName;
+        public readonly string LastName;
+        public readonly DateTime DateOfBirth;
+        public readonly string JobTitle;
+
+        public CreateAgentCommand(Guid id, int agentID, string firstName, string lastName, DateTime dateOfBirth, string jobTitle)
+        {
+            Id = id;
+            AgentID = agentID;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            JobTitle = jobTitle;
+        }
+    }
+}

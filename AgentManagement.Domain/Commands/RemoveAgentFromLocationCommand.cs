fee957ea912f222342373f8fa2d6f038f2d498a8
eMateyWalid <walidhefhaf@gmail.com>
2019-08-18 18:38:48 +0100

    Command resposnsible of removing an agent from a location.

---
+++ AgentManagement.Domain/Commands/RemoveAgentFromLocationCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AgentManagement.Domain.Commands
+{
+    public class RemoveAgentFromLocationCommand : BaseCommand
+    {
+        public readonly int AgentID;
+        public readonly int LocationID;
+
+        public RemoveAgentFromLocationCommand(Guid id, int locationID, int agentID)
+        {
+            Id = id;
+            AgentID = agentID;
+            LocationID = locationID;
+        }
+    }
+}

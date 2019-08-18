abc0045de4242bd8317c279989d5e56bf6b273c7
eMateyWalid <walidhefhaf@gmail.com>
2019-08-18 19:31:28 +0100

    Basic implementation of use cases in regards of MergeIntoAllCollection method that merged back into the respective "all collection" for the object lastly saved.

---
+++ AgentManagement.Domain/ReadModel/Repositories/AgentRepository.cs
@@ -0,0 +1,53 @@
+﻿using AgentManagement.Domain.ReadModel.Repositories.Abstract;
+using StackExchange.Redis;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AgentManagement.Domain.ReadModel.Repositories
+{
+    public class AgentRepository : BaseRepository, IAgentRepository
+    {
+        public AgentRepository(IConnectionMultiplexer redisConnection) : base(redisConnection, "agent") { }
+
+        public AgentRM GetByID(int agentID)
+        {
+            return Get<AgentRM>(agentID);
+        }
+
+        public List<AgentRM> GetMultiple(List<int> agentIDs)
+        {
+            return GetMultiple<AgentRM>(agentIDs);
+        }
+
+        public IEnumerable<AgentRM> GetAll()
+        {
+            return Get<List<AgentRM>>("all");
+        }
+
+        public void Save(AgentRM agent)
+        {
+            Save(agent.AgentID, agent);
+            MergeIntoAllCollection(agent);
+        }
+
+        private void MergeIntoAllCollection(AgentRM agent)
+        {
+            List<AgentRM> allAgents = new List<AgentRM>();
+            if (Exists("all"))
+            {
+                allAgents = Get<List<AgentRM>>("all");
+            }
+
+            //If the district already exists in the ALL collection, remove that entry
+            if (allAgents.Any(x => x.AgentID == agent.AgentID))
+            {
+                allAgents.Remove(allAgents.First(x => x.AgentID == agent.AgentID));
+            }
+
+            //Add the modified district to the ALL collection
+            allAgents.Add(agent);
+
+            Save("all", allAgents);
+        }
+    }
+}
---
+++ AgentManagement.Domain/ReadModel/Repositories/LocationRepository.cs
@@ -0,0 +1,66 @@
+﻿using AgentManagement.Domain.ReadModel.Repositories.Abstract;
+using StackExchange.Redis;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AgentManagement.Domain.ReadModel.Repositories
+{
+    public class LocationRepository : BaseRepository, ILocationRepository
+    {
+        public LocationRepository(IConnectionMultiplexer redisConnection) : base(redisConnection, "location") { }
+
+        public LocationRM GetByID(int locationID)
+        {
+            return Get<LocationRM>(locationID);
+        }
+
+        public List<LocationRM> GetMultiple(List<int> locationIDs)
+        {
+            return GetMultiple(locationIDs);
+        }
+
+        public bool HasAgent(int locationID, int agentID)
+        {
+            //Deserialize the LocationDTO with the key location:{locationID}
+            var location = Get<LocationRM>(locationID);
+
+            //If that location has the specified Agent, return true
+            return location.Agents.Contains(agentID);
+        }
+
+        public IEnumerable<LocationRM> GetAll()
+        {
+            return Get<List<LocationRM>>("all");
+        }
+        public IEnumerable<AgentRM> GetAgents(int locationID)
+        {
+            return Get<List<AgentRM>>(locationID.ToString() + ":agents");
+        }
+
+        public void Save(LocationRM location)
+        {
+            Save(location.LocationID, location);
+            MergeIntoAllCollection(location);
+        }
+
+        private void MergeIntoAllCollection(LocationRM location)
+        {
+            List<LocationRM> allLocations = new List<LocationRM>();
+            if (Exists("all"))
+            {
+                allLocations = Get<List<LocationRM>>("all");
+            }
+
+            //If the district already exists in the ALL collection, remove that entry
+            if (allLocations.Any(x => x.LocationID == location.LocationID))
+            {
+                allLocations.Remove(allLocations.First(x => x.LocationID == location.LocationID));
+            }
+
+            //Add the modified district to the ALL collection
+            allLocations.Add(location);
+
+            Save("all", allLocations);
+        }
+    }
+}

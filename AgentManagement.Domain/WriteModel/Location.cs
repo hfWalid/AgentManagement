0771203e15d250537f5505989124dbdcd48a907a
eMateyWalid <walidhefhaf@gmail.com>
2019-08-18 18:30:36 +0100

    Aggregate Root object defined for the Location object.

---
+++ AgentManagement.Domain/WriteModel/Location.cs
@@ -0,0 +1,50 @@
+﻿using AgentManagement.Domain.Events.Locations;
+using CQRSlite.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AgentManagement.Domain.WriteModel
+{
+    public class Location : AggregateRoot
+    {
+        private int _locationID;
+        private string _streetAddress;
+        private string _city;
+        private string _state;
+        private string _postalCode;
+        private List<int> _agents;
+
+        private Location() { }
+
+        public Location(Guid id, int locationID, string streetAddress, string city, string state, string postalCode)
+        {
+            Id = id;
+            _locationID = locationID;
+            _streetAddress = streetAddress;
+            _city = city;
+            _state = state;
+            _postalCode = postalCode;
+            _agents = new List<int>();
+
+            ApplyChange(new LocationCreatedEvent(id, locationID, streetAddress, city, state, postalCode));
+        }
+
+        public void AddAgent(int agentID)
+        {
+            _agents.Add(agentID);
+            ApplyChange(new AgentAssignedToLocationEvent(Id, _locationID, agentID));
+        }
+
+        public static explicit operator Location(Task<Location> v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAgent(int agentID)
+        {
+            _agents.Remove(agentID);
+            ApplyChange(new AgentRemovedFromLocationEvent(Id, _locationID, agentID));
+        }
+    }
+}

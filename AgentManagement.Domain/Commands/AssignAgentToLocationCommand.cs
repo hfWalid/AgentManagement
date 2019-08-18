46dcb2f1767f2dde66dbe8042d0f8941af75bf51
eMateyWalid <walidhefhaf@gmail.com>
2019-08-18 18:38:17 +0100

    Command responsible of the assigning of an agent to a /new location

---
+++ AgentManagement.Domain/Commands/AssignAgentToLocationCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AgentManagement.Domain.Commands
+{
+    public class AssignAgentToLocationCommand : BaseCommand
+    {
+        public readonly int AgentID;
+        public readonly int LocationID;
+
+        public AssignAgentToLocationCommand(Guid id, int locationID, int agentID)
+        {
+            Id = id;
+            AgentID = agentID;
+            LocationID = locationID;
+        }
+    }
+}

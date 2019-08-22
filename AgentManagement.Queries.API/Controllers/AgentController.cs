db27b44216507c4b6113a03eda2c3f2a625a708a
eMateyWalid <walidhefhaf@gmail.com>
2019-08-22 20:53:20 +0100

    To execute GetById and GetAll queries

---
+++ AgentManagement.Queries.API/Controllers/AgentController.cs
@@ -0,0 +1,41 @@
+﻿using AgentManagement.Domain.ReadModel.Repositories.Abstract;
+using System.Web.Http;
+
+namespace AgentManagement.Queries.API.Controllers
+{
+    [RoutePrefix("Agents")]
+    public class AgentController : ApiController
+    {
+        private readonly IAgentRepository _agentRepo;
+
+        public AgentController(IAgentRepository agentRepo)
+        {
+            _agentRepo = agentRepo;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IHttpActionResult GetByID(int id)
+        {
+            var agent = _agentRepo.GetByID(id);
+
+            //It is possible for GetByID() to return null.
+            //If it does, we return HTTP 400 Bad Request
+            if (agent == null)
+            {
+                return BadRequest("No Agent with ID " + id.ToString() + " was found.");
+            }
+
+            //Otherwise, we return the agent
+            return Ok(agent);
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public IHttpActionResult GetAll()
+        {
+            var agents = _agentRepo.GetAll();
+            return Ok(agents);
+        }
+    }
+}
---
+++ AgentManagement.Queries.API/Controllers/LocationController.cs
@@ -0,0 +1,44 @@
+﻿using AgentManagement.Domain.ReadModel.Repositories.Abstract;
+using System.Web.Http;
+
+namespace AgentManagement.Queries.API.Controllers
+{
+    [RoutePrefix("location")]
+    public class LocationController : ApiController
+    {
+        private ILocationRepository _locationRepo;
+
+        public LocationController(ILocationRepository locationRepo)
+        {
+            _locationRepo = locationRepo;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IHttpActionResult GetByID(int id)
+        {
+            var location = _locationRepo.GetByID(id);
+            if (location == null)
+            {
+                return BadRequest("No location with ID " + id.ToString() + " was found.");
+            }
+            return Ok(location);
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public IHttpActionResult GetAll()
+        {
+            var locations = _locationRepo.GetAll();
+            return Ok(locations);
+        }
+
+        [HttpGet]
+        [Route("{id}/agents")]
+        public IHttpActionResult GetAgents(int id)
+        {
+            var agents = _locationRepo.GetAgents(id);
+            return Ok(agents);
+        }
+    }
+}

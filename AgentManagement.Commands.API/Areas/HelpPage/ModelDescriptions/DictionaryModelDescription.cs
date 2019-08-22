3642cb2a95ddd3439c377169df7f7a0022eba1b2
eMateyWalid <walidhefhaf@gmail.com>
2019-08-22 20:57:42 +0100

    API can issue commands to the system.

---
+++ AgentManagement.Commands.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AgentManagement.Commands.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AgentManagement.Commands.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AgentManagement.Commands.API.WebApiApplication" Language="C#" %>
